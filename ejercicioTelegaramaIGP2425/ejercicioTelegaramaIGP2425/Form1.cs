7081dae9e903ce9d16e580c3b2d3e6b6ecd20976
Isabel Granados Palma <isagrapal@alu.edu.gva.es>
2024-11-13 13:24:26 +0100

    Primer guardado sin código.

---
+++ ejercicioTelegaramaIGP2425/ejercicioTelegaramaIGP2425/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicioTelegaramaIGP2425
+{
+    public partial class FormEjercicioTelegramaIGP2425 : Form
+    {
+        public FormEjercicioTelegramaIGP2425()
+        {
+            InitializeComponent();
+        }
+    }
+}
